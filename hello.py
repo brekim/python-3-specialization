025e2e9e59a772bbac36f016abc316f69c9605bc
Brett Kim <bkim2021@francisparker.org>
2020-06-19 14:57:18 -0700

    First Python work

---
+++ hello.py
@@ -0,0 +1,23 @@
+# hello.py
+
+# Print statements, comments, values, and data types
+print("Hello World")
+
+print(2 + 2)
+print(2 * 2)
+print(18 % 4)
+
+almonds = 42
+elective = "Interpret Dance Using Python 3"
+
+print(type(elective))
+print(type(str(almonds)))
+
+print("Yes, this is it! " + str(almonds) + " almonds!")
+
+a = 6
+b = a
+a += 2
+
+print(a) # Should be 8
+print(b) # Should be 6
