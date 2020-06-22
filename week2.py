085c01f02e7e8b15e6af1a7bbe4947da7c5855a4
Brett Kim <bkim2021@francisparker.org>
2020-06-21 23:16:30 -0700

    Finished Week 2 assignments

--- week2.py
+++ week2.py
@@ -16,3 +16,18 @@ firstLetterInUsername = username[0]
 print(firstLetterInUsername)
 lastLetterInUsername = username[len(username) - 1]
 print(lastLetterInUsername)
+
+players = ["device", "s1mple", "mertz"]
+mouz2018 = ["sunNy", "ropz", "chrisJ", "oskar", "STYKO"]
+
+print(mouz2018[:2])
+print(mouz2018[2:])
+print(mouz2018 * 5)
+
+print("I guess we're getting rid of the " + str(mouz2018.index("sunNy") + 1) + "st player on the team.")
+
+allPlayers = players + mouz2018
+print(allPlayers)
+
+allMouzPlayers = '<3'.join(mouz2018)
+print(allMouzPlayers)
