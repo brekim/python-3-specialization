8f020c6e68dad10b19339cee68ddd978e5ba3358
Brett Kim <bkim2021@francisparker.org>
2020-06-30 23:46:13 -0700

    Week 5 complete

--- week5.py
+++ week5.py
@@ -1,7 +1,7 @@
 # week5.py
 
 # Reading
-file = open(".\\check.txt", "r")
+file = open(".\\check.csv", "r")
 content = file.readlines()
 print(content)
 
@@ -11,15 +11,16 @@ for line in file:
 file.close()
 
 # Writing
-file = open(".\\check.txt", "a") # w to change it
+file = open(".\\check.csv", "a") # w to change it
 
 file.write("\nExample,Country,0,0,0.0,1.05")
 
 file.close()
 
 # Using with to open Python files
-with open('check.txt') as new_file:
+with open('check.csv') as new_file:
     lines = new_file.readlines()
     for line in lines:
         sp = line.split(",")
         print("{} - {}. Rating: {} K: {} D: {} ADR: {}".format(sp[0], sp[1], sp[5], sp[2], sp[3], sp[4]))
+
