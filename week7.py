e4a6292d9f8c6bff9807d839048a0de153c0d25b
Brett Kim <bkim2021@francisparker.org>
2020-08-06 11:50:01 -0700

    Week 7 progress

--- week7.py
+++ week7.py
@@ -1,8 +1,11 @@
 # Author: Brett Kim
 
+global_var = 1
+
 def hello(name, count):
+    local_var = 1
     greeting = "Hello {}!\n".format(name)
-    print(greeting*count)
+    print(greeting*(count * local_var))
 
 def square(number):
     return number * number
@@ -15,5 +18,14 @@ def sum_of_list(lst):
         sum += i
     return sum
 
+def sum_of_squares(lst):
+    sum = 0
+    for i in lst:
+        sum += square(i)
+    return sum
+
+# sum is local
+
 br = [2, 4, 6, 8]
 print(sum_of_list(br))
+print(sum_of_squares(br))
