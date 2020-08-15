5b07acab751e4971550377c4af6fe223d9a5f32f
Brett Kim <bkim2021@francisparker.org>
2020-08-14 18:19:39 -0700

    Beginning week 9

---
+++ week9.py
@@ -0,0 +1,7 @@
+# Author: Brett Kim
+
+random_one_to_ten = [10, 5, 1, 6, 4, 7, 9, 3, 2, 8]
+
+print(random_one_to_ten)
+print(sorted(random_one_to_ten))
+print(sorted(random_one_to_ten, reverse=True))
