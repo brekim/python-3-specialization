6f64795f066057392f0aa81f847263bbfef5610d
Brett Kim <31803504+brekim@users.noreply.github.com>
2020-08-15 19:33:42 -0700

    Week 9 progress, finished sorting section

--- week9.py
+++ week9.py
@@ -5,3 +5,12 @@ random_one_to_ten = [10, 5, 1, 6, 4, 7, 9, 3, 2, 8]
 print(random_one_to_ten)
 print(sorted(random_one_to_ten))
 print(sorted(random_one_to_ten, reverse=True))
+
+def absolute(x):
+  if x >= 0:
+    return x
+  else:
+    return -x
+
+win_diff = [93, -3, -58, -3, 49, 10, 3, 48]
+print(sorted(win_diff, reverse=True, key=absolute)
