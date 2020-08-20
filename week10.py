86502f733e315ade7d7be15974519cd2611b15ce
Brett Kim <bkim2021@francisparker.org>
2020-08-20 13:39:15 -0700

    Week 10 working

--- week10.py
+++ week10.py
@@ -5,3 +5,14 @@ mavs = [[33, 42], [5, 1], [42, 4]]
 for lst in mavs:
     for score in lst:
         print(score)
+
+rockets = {'players':
+            {'name': 'James Harden',
+            'points': 40,
+            'assists': 8,
+            'rebounds': 6,
+            'physical_features':
+            {'beard': True,
+            'bald': False}}}
+
+print(rockets['players']['points'])
