04799b8ea11cb21c877904da09391b83abbd9793
Brett Kim <bkim2021@francisparker.org>
2020-08-21 13:49:56 -0700

    Nested dictionaries for week 10

--- week10.py
+++ week10.py
@@ -11,8 +11,23 @@ rockets = {'players':
             'points': 40,
             'assists': 8,
             'rebounds': 6,
-            'physical_features':
-            {'beard': True,
-            'bald': False}}}
+            'physical_features': {'beard': True,
+                                    'bald': False}
+            }
+        }
 
 print(rockets['players']['points'])
+
+malta = {'events':
+            {
+            2019: {'name': 'ESL Cologne',
+            'winner': 'Astralis'
+            },
+            2020: {'name': 'ELEAGUE Boston',
+            'winner': 'Cloud9',
+            'runnerup': 'FaZe'}
+            }
+        }
+
+print(malta['events'][2019]['winner'])
+print(malta['events'][2020]['winner'])
